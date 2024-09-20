8cda54d367f7a07a506044151c2f739c91efb495
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-09-20 21:49:31 +0200

    orthodox canonical form for Shrubbery

--- M05/ex02/ShrubberyCreationForm.cpp
+++ M05/ex02/ShrubberyCreationForm.cpp
@@ -2,11 +2,30 @@
 #include <fstream>
 #include <iostream>
 
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm("Shrubbery Creation Form", 147, 137), _target("Default Target")
+{
+}
+
 ShrubberyCreationForm::ShrubberyCreationForm(const std::string &target) : AForm("Shrubbery Creation", 145, 137), _target(target)
 {
 }
+
 ShrubberyCreationForm::~ShrubberyCreationForm()
 {
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &other) : AForm(other), _target(other._target)
+{
+}
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm &other)
+{
+	if (this != &other)
+	{
+		AForm::operator=(other);
+		_target = other._target;
+	}
+	return *this;
 }
 
 void ShrubberyCreationForm::execute(Bureaucrat const &executor) const
--- M05/ex02/ShrubberyCreationForm.hpp
+++ M05/ex02/ShrubberyCreationForm.hpp
@@ -7,10 +7,16 @@ class ShrubberyCreationForm : public AForm
 {
 	private:
 		std::string _target;
-
+		ShrubberyCreationForm(); // default constructor I put it there because Igor told me to xD
+				// supposedly it should be for the compiler,
+				// so we can't access it directly, but the compiler doesnt
+				// create its own
 	public:
-		ShrubberyCreationForm(const std::string &target);
-		virtual ~ShrubberyCreationForm();
+		ShrubberyCreationForm(const std::string &target); // constructor
+		virtual ~ShrubberyCreationForm(); // destructor
+		ShrubberyCreationForm(const ShrubberyCreationForm &other); // copy constructor
+		ShrubberyCreationForm &operator=(const ShrubberyCreationForm &other); // copy assignment operator
+
 		void execute(Bureaucrat const &executor) const;
 };
 
