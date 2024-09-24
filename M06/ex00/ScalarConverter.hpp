64eef9760c088625f80c29dc079f602ffd03e531
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-09-24 20:27:16 +0200

    initial setup and thoughts

---
+++ M06/ex00/ScalarConverter.hpp
@@ -0,0 +1,21 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+// As this class doesn’t need to store anything at all, this class must not be instanciable by
+// users.
+
+// So probably abstract class and after that a class for every type char, int, float, double,
+// i suspect
+
+// one static method "convert
+
+// maybe take string as an identifier for the datatype?
+class ScalarConverter
+{
+	private:
+		ScalarConverter(); // default constructor
+
+	public:
+};
+
+#endif
