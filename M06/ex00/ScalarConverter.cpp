b97d3c92dc6d5f0ee5323c6ad542019995e252ae
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-09-24 21:59:41 +0200

    something

--- M06/ex00/ScalarConverter.cpp
+++ M06/ex00/ScalarConverter.cpp
@@ -1,1 +1,37 @@
 #include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() : _literal("c")
+{
+	// std::cout << "ScalarConverter default constructor called" << std::endl;
+}
+
+ScalarConverter::ScalarConverter(const std::string &literal) : _literal(literal)
+{
+	// TODO add exceptions for checking the format
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &other) : _literal(other._literal)
+{
+
+}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other)
+{
+	if (this != &other)
+	{
+		// probably nothing
+	}
+	return *this;
+}
+
+ScalarConverter::~ScalarConverter()
+{
+
+}
+
+// Exceptions
+// TODO maybe provide right format
+const char *ScalarConverter::LiteralInWrongFormat::what() const throw()
+{
+	return "Literal is in wrong format";
+}
--- M06/ex00/ScalarConverter.hpp
+++ M06/ex00/ScalarConverter.hpp
@@ -9,13 +9,35 @@
 
 // one static method "convert
 
-// maybe take string as an identifier for the datatype?
+#include <iostream>
+
 class ScalarConverter
 {
 	private:
+		const std::string _literal;
 		ScalarConverter(); // default constructor
 
 	public:
+		ScalarConverter(const std::string &literal); // constructor
+		ScalarConverter(const ScalarConverter &other); // copy constructor
+		ScalarConverter &operator=(const ScalarConverter &other); // copy assignment operator
+		virtual ~ScalarConverter();
+
+		std::string getLiteral() const; // probably cant have this thanks to subject ? maybe
+
+		// Write a class ScalarConverter that will contain only one static method "convert
+		// only one?
+
+		virtual void convert(const std::string &literal) const = 0;
+
+		// excpetions
+		// TODO
+
+		class LiteralInWrongFormat : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
 };
 
 #endif
