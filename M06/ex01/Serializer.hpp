a45725b90e56141cc5ffff28cc7846546c1b2a4a
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-09-27 20:44:37 +0200

    something

--- M06/ex01/Serializer.hpp
+++ M06/ex01/Serializer.hpp
@@ -1,6 +1,9 @@
 #ifndef SERIALIZER_HPP
 #define SERIALIZER_HPP
 
+#include <stdint.h>
+#include "Data.hpp"
+
 class Serializer
 {
 	private:
@@ -11,7 +14,8 @@ class Serializer
 		~Serializer(); // destructor
 	
 	public:
-
+		// It takes a pointer and converts it to the unsigned integer type uintptr_t.
+		uintptr_t serialize(Data* ptr);
 		// add errors
 };
 #endif
