f9eb42794f34f4582e2fecea4c51b532b6596e42
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-09-30 17:58:06 +0200

    just to be sure trycatch

--- M06/ex02/main.cpp
+++ M06/ex02/main.cpp
@@ -12,15 +12,33 @@ int main()
 {
 	Base* base = generate();
 
+	if (base == NULL)
+	{
+		std::cerr << "Failed to generate a valid Base object." << std::endl;
+	}
 	// identify by pointer
 	std::cout << "Identify by pointer: ";
-	identify(base);
-
+	try
+	{
+		identify(base);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
 	// identify by reference
 	std::cout << "Identify by reference: ";
-	identify(*base);
-
+	try
+	{
+		identify(*base);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
 	delete base;
 
-	return 0; // probably do try catch, but we will see
+	return 0;
 }
