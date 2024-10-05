3bd38ca4474ee815c8634f31c3089f1d6cf1af1f
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-10-05 18:00:15 +0200

    deleted some useless stuff

--- M07/ex00/Whatever.hpp
+++ M07/ex00/Whatever.hpp
@@ -1,22 +1,6 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-// probably delete 
-// your CLASSES must be designed in the Orthodox Canonical Form
-class Whatever
-{
-	private:
-	// change later to public based on what i need
-		Whatever(); // default constructor
-		Whatever(const Whatever &other); // copy constructor
-		Whatever &operator=(const Whatever &other); // copy assignement operator
-		~Whatever(); // destructor
-
-	public:
-	// min max swap later
-	// errors
-};
-
 template <typename T>
 void swap(T &a, T &b)
 {
