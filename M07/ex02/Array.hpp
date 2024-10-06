070f0cc78b2ff3f54762cf889099303f68b9e4f5
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-10-06 19:27:31 +0200

    including broken

--- M07/ex02/Array.hpp
+++ M07/ex02/Array.hpp
@@ -1,16 +1,36 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+
+template <typename T>
 class Array
 {
 	private:
+		T* elements;
+		unsigned int n;
+	
+	public:
 		Array(); // default constructor
+		Array(unsigned int n); // parameterized constructor
 		Array(const Array &other); // copy constructor
 		Array &operator=(const Array &other); // copy assignment operator
 		~Array(); // destructor
-	
-	public:
 		
+		T &operator[](unsigned int index); // subscript operator
+		const T &operator[](unsigned int index) const; // const subscript operator
+		unsigned int size() const; // size function
+
+		// exceptions
+		class OutOfBounds : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+
 };
+// should be down otherwise it will not find the class
+// i can put everything in header
+#include "Array.tpp"
 
 #endif
---
+++ M07/ex02/Array.tpp
@@ -0,0 +1,4 @@
+#include "Array.hpp"
+
+template <typename T>
+Array<T>::Array() : elements(NULL), n(0) {}
