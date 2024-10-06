afa811ad174d2bb3105c7dcc12a4d3329407e142
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-10-06 16:19:44 +0200

    preparing

---
+++ M07/ex02/Array.hpp
@@ -0,0 +1,16 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+class Array
+{
+	private:
+		Array(); // default constructor
+		Array(const Array &other); // copy constructor
+		Array &operator=(const Array &other); // copy assignment operator
+		~Array(); // destructor
+	
+	public:
+		
+};
+
+#endif
