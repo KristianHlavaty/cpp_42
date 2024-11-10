8cfd18121cc811e27af4b1451a8289186077b99c
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-11-11 00:26:56 +0100

    I think that is passable ex00

--- M08/ex00/easyfind.hpp
+++ M08/ex00/easyfind.hpp
@@ -3,11 +3,22 @@
 
 #include <iostream>
 
+// need typename T::value_type, T* would work only for raw arrays
 template<typename T>
-void easyfind(T containerOfIntegers, int integer)
+typename T::value_type* easyFind(T &containerOfIntegers, int integer)
 {
-	(void)containerOfIntegers;
-	std::cout << "HERE " << integer << std::endl;
-	// lets try vector once i get back
+	if(containerOfIntegers.empty())
+	{
+		throw std::runtime_error("Container is empty"); // so it doesnt segfaults with empty vector
+	}
+	for(typename T::iterator it = containerOfIntegers.begin(); it != containerOfIntegers.end(); ++it)
+	{
+		if(*it == integer)
+		{
+			return &(*it);
+		}
+	}
+	// if didnt find
+	throw std::runtime_error("Value not found in container");
 }
 #endif
--- M08/ex00/main.cpp
+++ M08/ex00/main.cpp
@@ -1,8 +1,95 @@
 #include <iostream>
+#include <vector>
 #include "easyfind.hpp"
 
 int main()
 {
-	std::cout << "test" << std::endl;
-	easyfind(NULL, 1);
+	int intArr[] = {-100, 2, 3, 3, 5, 6};
+	int size = sizeof(intArr) / sizeof(intArr[0]);
+
+	// intArr 			-> pointer to first elem.
+	// intArr + size 	-> pointer to one past the end
+	std::vector<int> intVector(intArr, intArr + size);
+	
+	//testing if the values are there
+	std::cout << "should print 6 numbers" << std::endl;
+	for (std::vector<int>::iterator it = intVector.begin(); it != intVector.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+	// Vector test
+	std::cout << std::endl;
+	std::cout << "Testing vector with number that is in the Array" << std::endl;
+	try
+	{
+		int* result = easyFind(intVector, 2);
+		std::cout << "Found value: " << *result << std::endl;
+	}
+	catch (const std::runtime_error &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << "Testing vector with number that is negative" << std::endl;
+	try
+	{
+		int* resultNeg = easyFind(intVector, -100);
+		std::cout << "Found value: " << *resultNeg << std::endl;
+	}
+	catch (const std::runtime_error &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+	std::cout << "Testing vector with number that is there twice" << std::endl;
+	try
+	{
+		int* resultTwice = easyFind(intVector, 3);
+		std::cout << "Found value: " << *resultTwice << std::endl;
+	}
+	catch (const std::runtime_error &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
+	std::vector<int> emptyVector;
+	std::vector<int> singleElementVector;
+	singleElementVector.push_back(5);
+	std::cout << std::endl;
+	std::cout << "Vector Error Section" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Testing empty vector" << std::endl;
+	try
+	{
+		int* emptySearch = easyFind(emptyVector, 3);
+		std::cout << "Found value: " << *emptySearch << std::endl;
+	}
+	catch (const std::runtime_error &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+	std::cout << "Testing value in single Elem Vector" << std::endl;
+	try
+	{
+		int* singleSearch = easyFind(singleElementVector, 5);
+		std::cout << "Found value: " << *singleSearch << std::endl;
+	}
+	catch (const std::runtime_error &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << "Testing value not in single Elem Vector" << std::endl;
+	try
+	{
+		int* singleSearchNotThere = easyFind(singleElementVector, 3);
+		std::cout << "Found value: " << *singleSearchNotThere << std::endl;
+	}
+	catch (const std::runtime_error &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
 }
