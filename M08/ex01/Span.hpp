5bb173dd856e127ac4e0cb680e10573a9ff79670
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-11-13 02:52:37 +0100

    nic

--- M08/ex01/Span.hpp
+++ M08/ex01/Span.hpp
@@ -6,16 +6,15 @@
 class Span
 {
 	private:
-		unsigned int n;
-		Span(); // probably dont have to be public, maybe
-				// default constructor
+		unsigned int n; // max number of integers
+		Span(); // probably dont have to be public
 	
 	public:
 		Span(unsigned int n); // parameterized constructor
 		Span(const Span &other); // copy constructor
 		Span &operator=(const Span &other); // copy assignment operator
 		~Span();
-
+		void addNumber();
 
 };
 
