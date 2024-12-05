95f97552f2b6caf814739e1ccd47798e7e805b4d
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-12-06 00:09:55 +0100

    just reading lol

--- M09/ex00/main.cpp
+++ M09/ex00/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <map>
+#include <fstream> //check if i can actually use it in cpp98
 
 int main()
 {
 	std::cout << "test" << std::endl;
+	
+	std::fstream fin;
+
+	fin.open("data.csv", std::ios::in);
+
 }
