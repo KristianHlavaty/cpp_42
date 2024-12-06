ab88b3841d63cf3dff585c72a3a882d4be247e67
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-12-06 20:03:17 +0100

    now i know how to read from csv file

--- M09/ex00/main.cpp
+++ M09/ex00/main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <map>
 #include <fstream> //check if i can actually use it in cpp98
+#include <sstream>
+#include <vector>
 
 int main()
 {
-	std::cout << "test" << std::endl;
+	std::cout << "reading csv file" << std::endl;
 	
 	std::fstream fin;
 
 	fin.open("data.csv", std::ios::in);
 
+	if(!fin.is_open())
+	{
+		std::cerr << "Error: Could not open file." << std::endl;
+	}
+
+	std::vector<std::string> row;
+	std::string line, word;
+	while(getline(fin, line))
+	{
+		row.clear();
+		std::stringstream ss(line);
+		while(getline(ss, word, ','))
+		{
+			row.push_back(word);
+		}
+		for(size_t j = 0; j < row.size(); j++)
+		{
+			std::cout << "Field" << j + 1 << row[j] << std::endl;
+		}
+	}
+	fin.close();
+	return 0;
 }
